f8dcf543b445215d7e88160b66b736bd7ad71d27
Filipe Molina <filipemolina@live.com>
2015-08-25 18:02:08 -0300

    Funções Helpers no Javascript principal

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+// Função para retornar preencher um select com todas as concessionárias de uma montadora
+
+// id_select = Identificador do select (da mesma forma como se fosse usando na função $() do jQuery)
+// montadora = ID da montadora de acordo com o cadastro no banco de dados
+
+function preencheConcessionarias(id_select, montadora)
+{
+	$.get("includes/concessionarias.php", { montadora : montadora }, function(data) 
+	{ 
+
+		var dados = JSON.parse(data);
+
+		for(dado in dados)
+		{
+			// Criar as options dentro do select
+
+			$(id_select).append("<option value='"+dados[dado].id+"'>"+dados[dado].nome+"</option>");
+		}
+
+	});
+}
+
+// Função para retornar preencher um select com todas as concessionárias de uma montadora
+
+// id_select = Identificador do select (da mesma forma como se fosse usando na função $() do jQuery)
+// montadora = ID da montadora de acordo com o cadastro no banco de dados
+
+function preencheCarros(id_select, montadora)
+{
+	$.get("includes/concessionarias.php", { montadora : montadora }, function(data) 
+	{ 
+
+		var dados = JSON.parse(data);
+
+		for(dado in dados)
+		{
+			// Criar as options dentro do select
+
+			$(id_select).append("<option value='"+dados[dado].id+"'>"+dados[dado].nome+"</option>");
+		}
+
+	});
+}
+
+$(function(){
+
+	///////////////////////////////////// As chamadas de eventos do jQuery devem ser inseridas aqui
+
+})
