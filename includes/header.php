13f546adda10bb7479177dfcf9a4902882ebe8d5
vinicius <pmachado.vinicius@gmail.com>
2015-08-26 04:22:02 -0300

    front end

--- includes/header.php
+++ includes/header.php
@@ -17,9 +17,30 @@
 		<!-- Estilos Padrão -->
 		<link rel="stylesheet" href="css/styles.css?v=1.0">
 
+		<!-- font source sans pro -->
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,700' rel='stylesheet' type='text/css'>
+
 		<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 	</head>
 
 	<body>
+	<!-- ---------------  MENU ---------------- -->
+	<div class="container">
+		<div class="row">
+			<nav class="menu">
+				<img src="img/logo_honda.png" height="64" width="82" alt="" class="col-md-1">
+				<ul class="col-md-9">
+					<li>Fan 125</li>
+					<li>Lead 110</li>
+					<li>XRE 330</li>
+					<li>Biz 125</li>
+					<li>CBR 650F</li>
+					<li>CB1000R</li>
+					<li>Dúvidas?</li>
+				</ul>
+				<a href="" class="col-md-2 class="btn-chat" ">Chat online</a>
+			</nav>
+
+	<!-- ----------------------- END MENU --------------------- -->
--- index.php
+++ index.php
@@ -1,23 +1,138 @@
 <?php require_once('includes/header.php'); ?>
 
 		<!-- Conteúdo -->
+
+		
+
+		</div>
+		</div>
+		<!-- ----- fim container e row ----- -->
+
+		<!-- baner estatico -->
+
+		<div class="banner"></div>
+
+		<!-- ---- marcas ---- -->
+
+		<h1 class="titulo-marca">VEJA OFERTAS DE OUTROS FABRICANTES</h1>
+		<div class="marcas-logos">
+			<ul>
+				<li><a href=""><img src="img/logo-chevrolet.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-fiat.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-ford.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-toyota.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-vw.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo_honda.png" alt=""></a></li>
+			</ul>
+		</div>
+		<div class="honda_banner-oferta_01"></div>
+		<div class="honda_banner-oferta_02"></div>
+		<div class="honda_banner-oferta_03"></div>
+
+
+
+		<div class="container">
+			<div class="formulario">
+				<h1 class="titulo-form"><span>PROPOSTA DE </span></br>FINANCIAMENTO</h1>
+			</div>
 		
 		<!-- Conteúdo temporário para testar o formulario de contatos -->
 
-		<form action="includes/cadastro.php" method="post">
+			<form action="includes/cadastro.php" method="post">
+
+				<div class="col-md-4">
+					<span class="name-forms">Nome</span>
+					<input type="text" class="form-control" name="nome"/>
+					<span class="name-forms">Email</span>
+					<input type="text" class="form-control" name="email"/>
+					<span class="name-forms">Telefone</span>
+					<input type="text" class="form-control" name="telefone"/>
+				</div>
 			
-			<input type="text" name="nome" placeholder="nome"/>
-			<input type="text" name="email" placeholder="email"/>
-			<input type="text" name="telefone" placeholder="telefone"/>
-			<input type="text" name="mensagem" placeholder="mensagem"/>
-			<input type="text" name="entrada" placeholder="entrada"/>
-			<input type="text" name="prestacoes" placeholder="prestacoes"/>
-			<input type="text" name="carro" placeholder="carro"/>
-			<input type="text" name="concessionaria" placeholder="concessionaria"/>
+				<div class="col-md-4">
+					<div class="ent-prest">
+						<div class="ent">
+						<span class="name-forms">Entranda</span>
+						<select class="form-entrada form-control" name="entrada">
+						<option value="10%">10%</option>
+						<option value="20%">20%</option>
+						<option value="30%">30%</option>
+						<option value="10%">40%</option>
+						<option value="20%">50%</option>
+						<option value="30%">60%</option>
+						<option value="10%">70%</option>
+						<option value="20%">80%</option>
+						<option value="30%">90%</option>
+						</select>
+						</div>
+						<div class="prest">
+						<span class="name-forms ">Prestações</span>
+						<select class="form-prestacao form-control" name="prestacoes">
+						<option value="12x">12x</option>
+						<option value="24x">24x</option>
+						<option value="36x">36x</option>
+						<option value="48x">48x</option>
+						<option value="60x">60x	</option>
+						</select>
+						</div>
+					</div>
+					<span class="name-forms">Selecione um carro</span>
+					<select class="form-control" id="car-honda" name="carro">
+					<option value="">1</option>
+					<option value="">2</option>
+					<option value="">3</option>
+					</select>
+					<span class="name-forms">Selecione uma concessionária</span>
+					<select class="form-control" name="concessionaria">
+					<option value="">1</option>
+					<option value="">2</option>
+					<option value="">3</option>
+					</select>
+				</div>
 
-			<input type="submit" value="Enviar">
+				<div class="col-md-4">
+				<span class="name-forms">Mensagem</span>
+					<input type="text" class="msg form-control" name="mensagem"/>
+				</div>
+				<input type="submit" class="enviar" value="Enviar">
 
-		</form>
+			</form>
+		</div>
+		<div class="container">
+			<h1 class="titulo-form"><span>TIRE SUAS </span></br>DÚVIDAS</h1>
+			<div class="tel col-md-6">
+				<h2>Telefone<img src="img/tel.jpg" style="margin-top:-20px;"></h2>
+				<p>Encontre a concessionária mais perto de você</p>
+				<select name=""class="form-control">
+					<<option value="">teste 123</option>}
+					option
+				</select>
+				<h3>Volkswagem Recreio - Rio de Janeiro, RJ</h3>
+				<h4>(21) 2176-9400</h4>
+			</div>
+			<div class="chat col-md-6">
+				<h2>Chat<img src="img/chat.jpg" style="margin-top:-20px;"></h2>
+				<p>Fale com os nossos vendedores online</p>
+				<a href="">Iniciar Chat</a>
+			</div>
+		</div>
+		<div class="container">	
+			<p class="texto-legal">Siena EL 1.0 completo 15/15, R$ 36.990,00 á vista ou entrada (R$23.300,00) + 48 parcelas de R$ 399,00. Taxa de 1,29%. CET de 19,13%. Total financiado R$ 42.486,56. Consulte as devidas taxas e condições com nossos vendedores. Crédito sujeito a aprovação da financeira. Promoções válidas enquanto durarem os estoques. Imagens meramente ilustrativas. Uno Vivace 2P 15/15: Uno Vivace Básica  2P 15/15, R$ 25.990,00 á vista ou entrada (R$18.400,00) + 60 parcelas deR$ 199,00. Taxa de 1,35%. CET de 21,38%. Total financiado R$ 30.354,40. Consulte as devidas taxas e condições com nossos vendedores. Crédito sujeito a aprovação da financeira. Promoções válidas enquanto durarem os estoques. Imagens meramente ilustrativas. Palio Fire 2P 15/16: Palio Fire Básica 2P 15/16, R$ 26.990,00 á vista ou entrada (R$16.740,00) + 48 parcelas de R$ 299,00. Taxa de 1,19%. CET de 19,13%. Total financiado R$ 31.105,44. Consulte as devidas taxas e condições com nossos vendedores. Crédito sujeito a aprovação da financeira. Promoções válidas enquanto durarem os estoques. Imagens meramente ilustrativas. Uno WAY 1.0 15/16 Nova Uno Way 1.0 15/16, R$ 36.990,00 á Vista. Consulte as devidas taxas e condições com nossos vendedores. Crédito sujeito a aprovação da financeira. Promoções válidas enquanto durarem os estoques. Imagens meramente ilustrativas. Strada Working 15/15: Strada Working 15/15, Básica  R$ 36.990,00 á vista ou entrada (R$23.540,00) + 48 parcelas de R$ 399,00. Taxa de 1,29%. CET de 20,20%. Total financiado R$ 42.696,32. Consulte as devidas taxas e condições com nossos vendedores. Crédito sujeito a aprovação da financeira. Promoções válidas enquanto durarem os estoques. Imagens meramente ilustrativas. Bravo Essence 15/16 Bravo Essence Completo 15/16, R$ 59.990,00 á Vista. Consulte as devidas taxas e condições com nossos vendedores. Crédito sujeito a aprovação da financeira. Promoções válidas enquanto durarem os estoques. Imagens meramente ilustrativas.</p>
+		</div>
+		<div class="container">
+			<div class="gl"><img src="img/logo_grupolider.jpg" align="center" alt=""></div>
+			<div class="marcas-logos2">
+			<ul>
+				<li><a href=""><img src="img/logo-chevrolet.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-fiat.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-ford.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-toyota.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo-vw.png" alt=""></a></li>
+				<li><a href=""><img src="img/logo_honda.png" alt=""></a></li>
+			</ul>
+		</div>
+		<div class="desen"><a href="https://3aworldwide.com.br"><img src="img/ass3a.jpg" height="16" width="154" alt=""></a></div>
+		</div>
 
 		<!-- Conteúdo temporário para testar o formulario de contatos -->
 
@@ -27,8 +142,8 @@
 
 			//////////////////////////////////////////////////// Exemplo de como preencher o select de concessionárias e carros:
 
-			//preencheConcessionarias("#select", '2');
-			//preencheCarros("#select", '2');
+			//preencheConcessionarias("#car-honda", '6');
+			//preencheCarros("#car-honda", '2');
 
 			// Substituir o 2 pelo id da montadora de acordo com o banco de dados
 
@@ -42,9 +157,9 @@
 
 			//////////////////////////////////////////////////// Como obter as concessionárias de uma montadora específica
 
-			// 	$.get('includes/concessionarias.php', { montadora : id_montadora }, function(data){ 
-			//		var dados = JSON.parse(data);
-			//	});
+				//$.get('includes/concessionarias.php', { montadora : 6 }, function(data){ 
+				//	var dados = JSON.parse(data);
+			//});
 
 			// Substituir 'id_montadora' por um integer que é o id da montadora no banco de dados.
 			// O resultado será o mesmo da função anterior
@@ -52,7 +167,6 @@
 		})
 
 		</script>
-
 
 <?php require_once('includes/footer.php'); ?>
 
