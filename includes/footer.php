d70eb840e706747cfa622a8f6598a83764829be4
Filipe Molina <filipemolina@live.com>
2015-08-26 16:37:22 -0300

    Ajustes

--- includes/footer.php
+++ includes/footer.php
@@ -8,5 +8,37 @@
 		<!-- Scripts da Página -->
 		<script src="js/scripts.js"></script>
 
+		<script type="text/javascript">
+
+			$(function(){
+
+				//////////////////////////////////////////////////// Exemplo de como preencher o select de concessionárias e carros:
+
+				//preencheConcessionarias("#select", '2');
+				//preencheCarros("#select", '2');
+
+				// Substituir o 2 pelo id da montadora de acordo com o banco de dados
+
+				//////////////////////////////////////////////////// Como obter todas as concessionárias:
+
+				// 	$.get('includes/concessionarias.php', function(data){ 
+				//		var dados = JSON.parse(data);
+				//	});
+
+				// A variavel dados será um vetor de objetos com as propriedades de acordo com o banco de dados (nome, endereco, telefone...)
+
+				//////////////////////////////////////////////////// Como obter as concessionárias de uma montadora específica
+
+				// 	$.get('includes/concessionarias.php', { montadora : id_montadora }, function(data){ 
+				//		var dados = JSON.parse(data);
+				//	});
+
+				// Substituir 'id_montadora' por um integer que é o id da montadora no banco de dados.
+				// O resultado será o mesmo da função anterior
+
+			})
+
+		</script>
+
 	</body>
 </html>
--- index.php
+++ index.php
@@ -21,38 +21,6 @@
 
 		<!-- Conteúdo temporário para testar o formulario de contatos -->
 
-		<script>
-
-		$(function(){
-
-			//////////////////////////////////////////////////// Exemplo de como preencher o select de concessionárias e carros:
-
-			//preencheConcessionarias("#select", '2');
-			//preencheCarros("#select", '2');
-
-			// Substituir o 2 pelo id da montadora de acordo com o banco de dados
-
-			//////////////////////////////////////////////////// Como obter todas as concessionárias:
-
-			// 	$.get('includes/concessionarias.php', function(data){ 
-			//		var dados = JSON.parse(data);
-			//	});
-
-			// A variavel dados será um vetor de objetos com as propriedades de acordo com o banco de dados (nome, endereco, telefone...)
-
-			//////////////////////////////////////////////////// Como obter as concessionárias de uma montadora específica
-
-			// 	$.get('includes/concessionarias.php', { montadora : id_montadora }, function(data){ 
-			//		var dados = JSON.parse(data);
-			//	});
-
-			// Substituir 'id_montadora' por um integer que é o id da montadora no banco de dados.
-			// O resultado será o mesmo da função anterior
-
-		})
-
-		</script>
-
 
 <?php require_once('includes/footer.php'); ?>
 
