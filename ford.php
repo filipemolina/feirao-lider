e2d3119947076785c8170461b9e803344aa7e907
Filipe Molina <filipemolina@live.com>
2015-08-26 21:24:55 -0300

    Ajustes

--- ford.php
+++ ford.php
@@ -56,7 +56,7 @@
 				<div class="col-md-4">
 					<div class="ent-prest">
 						<div class="ent">
-						<span class="name-forms">Entranda</span>
+						<span class="name-forms">Entrada</span>
 						<select class="form-entrada form-control" name="entrada" id="entrada">
 						<option value="10%">10%</option>
 						<option value="20%">20%</option>
@@ -174,6 +174,16 @@
 			});
 
 		</script>
+
+		<!--Start of Zopim Live Chat Script-->
+		<script type="text/javascript">
+		window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+		d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+		_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+		$.src="//v2.zopim.com/?3G8Gd569c7FceXPFoD4xCfay0AuzmTOA";z.t=+new Date;$.
+		type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+		</script>
+		<!--End of Zopim Live Chat Script-->
 
 
 <?php require_once('includes/footer.php'); ?>
